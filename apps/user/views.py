60bfc31192c09464a0b67482d738d441c409fd09
Safwan <s.saigh@puritec.sa>
2021-12-30 02:00:44 +0300

    custom exceptions

--- apps/user/views.py
+++ apps/user/views.py
@@ -6,11 +6,12 @@ from rest_framework.permissions import IsAuthenticated
 from .models import *
 from django.utils.translation import gettext_lazy as _
 from rest_framework.exceptions import ValidationError
-from core.errors import Errors
+from core.errors import Errors, APIError
 
 
 class UserView(APIView):
     
     def get(self, request):
-        raise ValidationError(**Errors.DEFAULT)
+        # raise ValidationError(**Errors.DEFAULT)
+        raise APIError(Errors.DEFAULT)
         return Response('hello')
--- core/errors.py
+++ core/errors.py
@@ -1,8 +1,15 @@
 from django.utils.translation import gettext_lazy as _
+from rest_framework.exceptions import ValidationError
 
 
-
+class APIError:
+    def __init__(self, error: dict, extra=None):
+        self.error = error
+        self.extra = extra or None
+        raise ValidationError(**error)
+        
+        
 class Errors:
-    DEFAULT = {"code": -100, "detail": "Oops!. Something went wrong, please contact us"}
+    DEFAULT = {"code": -100, "detail": _("Oops!. Something went wrong, please contact us")}
 
 
