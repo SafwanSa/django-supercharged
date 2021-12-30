afc362a6751827973439d5f28e5000ce964cbe8c
Safwan <s.saigh@puritec.sa>
2021-12-30 15:39:59 +0300

    enhancement: enhance error handling

--- apps/user/views.py
+++ apps/user/views.py
@@ -6,12 +6,11 @@ from rest_framework.permissions import IsAuthenticated
 from .models import *
 from django.utils.translation import gettext_lazy as _
 from rest_framework.exceptions import ValidationError
-from core.errors import Errors, APIError
+from core.errors import Error, APIError
 
 
 class UserView(APIView):
     
     def get(self, request):
-        # raise ValidationError(**Errors.DEFAULT)
-        raise APIError(Errors.DEFAULT)
+        raise APIError(Error._DEFAULT_MESSAGE, extra=["this is a message"])
         return Response('hello')
--- core/errors.py
+++ core/errors.py
@@ -1,15 +1,25 @@
+from typing import Iterable
 from django.utils.translation import gettext_lazy as _
 from rest_framework.exceptions import ValidationError
+from enum import Enum
 
 
+        
+        
+class Error(Enum):
+    DEFAULT = {"code": -100, "detail": _("Oops!. Something went wrong, please contact us")}
+    _DEFAULT_MESSAGE = {"code": 0, "detail": _("Default: {}")}
+    
+    
+    
 class APIError:
-    def __init__(self, error: dict, extra=None):
+    def __init__(self, error: Error, extra=None):
         self.error = error
         self.extra = extra or None
-        raise ValidationError(**error)
-        
-        
-class Errors:
-    DEFAULT = {"code": -100, "detail": _("Oops!. Something went wrong, please contact us")}
-
-
+        error_detail = error.value
+        if self.extra:
+            # Extra values can be used in foramtting a string that contains {}
+            if isinstance(self.extra, list):    
+                error_detail['detail'] = error_detail['detail'].format(*extra)
+                
+        raise ValidationError(**error_detail)
