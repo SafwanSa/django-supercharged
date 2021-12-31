3ace0dfbe3ca08b8eb9ed8aa36efa8874b5014f2
Safwan <s.saigh@puritec.sa>
2021-12-31 12:33:49 +0300

    chore: add a view descirption

--- apps/user/views.py
+++ apps/user/views.py
@@ -7,8 +7,9 @@ from rest_framework.permissions import IsAuthenticated
 from .models import *
 from django.utils.translation import gettext_lazy as _
 from core.errors import Error, APIError
-
+from rest_framework.pagination import PageNumberPagination
 
 class UserView(ListAPIView):
     queryset = User.objects.all()
     serializer_class = UserSerializer
+    description = "This endpoint return the list of users"
