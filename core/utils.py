b73e355c01a213cfcef6f1a29c6ab03948562e6b
Safwan <s.saigh@puritec.sa>
2022-03-02 20:45:40 +0300

    feat: add base model

--- core/utils.py
+++ core/utils.py
@@ -1,4 +1,5 @@
 import os
+from typing import Iterable
 import uuid
 import jwt
 from datetime import datetime, timedelta
@@ -7,6 +8,9 @@ import random
 from django.urls import reverse
 from django.utils.html import format_html
 from django.conf import settings
+from rest_framework import serializers
+from enum import Enum
+from django.utils.deconstruct import deconstructible
 
 
 class RandomFileName(object):
@@ -22,6 +26,7 @@ class RandomFileName(object):
         return self.path % (uuid.uuid4(), extension)
 
 
+@deconstructible
 class PathAndRename(object):
 
     def __init__(self, sub_path):
@@ -95,3 +100,50 @@ def linkify_instance(instance) -> str:
     view_name = f'admin:{app_label}_{model_name}_change'
     link_url = reverse(view_name, args=[linked_obj.pk])
     return format_html('<a href="{}">{}</a>', link_url, linked_obj)
+
+
+class DynamicFieldsModelSerializer(serializers.ModelSerializer):
+    """
+    A ModelSerializer that takes an additional `fields` and 'exclude' argument that
+    controls which fields should be displayed.
+    """
+
+    def __init__(self, *args, **kwargs):
+        # Don't pass the 'fields' arg up to the superclass
+        fields = kwargs.pop('fields', None)
+        exclude = kwargs.pop('exclude', None)
+
+        # Instantiate the superclass normally
+        super(DynamicFieldsModelSerializer, self).__init__(*args, **kwargs)
+
+        if fields is not None:
+            # Drop any fields that are not specified in the `fields` argument.
+            allowed = set(fields)
+            existing = set(self.fields.keys())
+            for field_name in existing - allowed:
+                self.fields.pop(field_name)
+
+        if exclude is not None:
+            not_allowed = set(exclude)
+            for exclude_name in not_allowed:
+                self.fields.pop(exclude_name)
+
+
+def create_serializer_class(name, fields):
+    return type(name, (serializers.Serializer, ), fields)
+
+
+def inline_serializer(*, fields, data=None, **kwargs):
+    serializer_class = create_serializer_class(name='', fields=fields)
+
+    if data is not None:
+        return serializer_class(data=data, **kwargs)
+
+    return serializer_class(**kwargs)
+
+
+def create_choices_from_enum(enum: Enum):
+    choices = []
+    for enum_value in enum:
+        choices.append((enum_value.value, (enum(enum_value.value).name).replace('_', ' ')))
+    return tuple(choices)
